b42d92826de49f8c937ea88e0f27b126130cb551
kittymiton <kitty.miton@gmail.com>
2026-04-19 22:27:24 +0900

    refactor: ローディングを機能毎に表示、コンポーネント化

--- src/app/(authenticated)/talkRoom/[id]/page.tsx
+++ src/app/(authenticated)/talkRoom/[id]/page.tsx
@@ -5,20 +5,18 @@ import { chatSchema } from '@/lib/schema/chatSchema';
 import { numberSchema } from '@/lib/validators/numberSchema';
 import { recipeSchema } from '@/lib/validators/recipeSchema';
 import { useSupabaseSession } from '@auth/hooks/useSupabaseSession';
-
+import { Loading } from '@authenticated/components/Loading';
 import { useAuthedSWR } from '@authenticated/hooks/useAuthedSWR';
 import { AsidePanel } from '@authenticated/talkRoom/components/AsidePanel';
 import { AsideRecipeList } from '@authenticated/talkRoom/components/AsideRecipeList';
 import { Button } from '@authenticated/talkRoom/components/Button';
 import { Suggest } from '@authenticated/talkRoom/components/Suggest';
 import { TalkInput } from '@authenticated/talkRoom/components/TalkInput';
-
 import { TalkList } from '@authenticated/talkRoom/components/TalkList';
 import { useSuggest } from '@authenticated/talkRoom/hooks/useSuggest';
 import { useTalkSubmit } from '@authenticated/talkRoom/hooks/useTalkSubmit';
 import { getSortedSuggestList } from '@authenticated/talkRoom/utils/getSortedSuggestList';
 import { runMutations } from '@authenticated/talkRoom/utils/runMutations';
-
 import Link from 'next/link';
 import { notFound, useParams } from 'next/navigation';
 import { useEffect, useRef, useState } from 'react';
@@ -26,13 +24,9 @@ import { mutate } from 'swr';
 
 export default function TalkRoomIdPage() {
   const [content, setContent] = useState<string>('');
-
   const [isFocused, setIsFocused] = useState(false);
-
   const inputRef = useRef<HTMLDivElement | null>(null);
-
   const { token } = useSupabaseSession();
-
   const params = useParams();
   const result = numberSchema.safeParse(params.id);
   if (!result.success) notFound();
@@ -67,32 +61,6 @@ export default function TalkRoomIdPage() {
     run,
     errorText,
   });
-
-  const isInitialLoading = !recipes || !talks;
-
-  let recipeContent;
-  const recipeIsEmpty = recipes?.length === 0;
-
-  if (!recipes) {
-    recipeContent = null;
-  } else if (recipeIsEmpty) {
-    recipeContent = <p>レシピがまだありません</p>;
-  } else {
-    recipeContent = (
-      <AsideRecipeList recipes={recipes} talkRoomId={talkRoomId} />
-    );
-  }
-
-  let talkContent;
-  const talkIsEmpty = talks?.length === 0;
-
-  if (!talks) {
-    talkContent = null;
-  } else if (talkIsEmpty) {
-    talkContent = <p>会話がまだありません</p>;
-  } else {
-    talkContent = <TalkList talks={talks} />;
-  }
 
   const handleFocus = () => {
     setIsFocused(true);
@@ -130,18 +98,23 @@ export default function TalkRoomIdPage() {
   return (
     <>
       <h1 className="text-lg font-bold mb-4">今日は何にしましょうか？</h1>
-
-      {isInitialLoading && <p>ローディング中...</p>}
       {errorMsg && <p className="text-red-500 mb-2">送信エラー：{errorMsg}</p>}
 
       <div className="flex h-[90vh]">
         <AsidePanel>
           <h2 className="font-bold mb-2">最近のレシピ</h2>
           <>
-            {recipeContent}
+            {!recipes && <Loading />}
+            {recipes &&
+              (recipes.length === 0 ? (
+                <p>レシピがまだありません</p>
+              ) : (
+                <AsideRecipeList recipes={recipes} talkRoomId={talkRoomId} />
+              ))}
             {url_asideError && (
               <p>取得に失敗しました: {url_asideError.message}</p>
             )}
+
             <Link
               href={`/recipes?from=${talkRoomId}`}
               className="text-sm underline text-blue-600"
@@ -152,7 +125,13 @@ export default function TalkRoomIdPage() {
         </AsidePanel>
 
         <main className="flex-1 flex flex-col">
-          {talkContent}
+          {!talks && <Loading />}
+          {talks &&
+            (talks.length === 0 ? (
+              <p>会話まだがありません</p>
+            ) : (
+              <TalkList talks={talks} />
+            ))}
           {url_mainError && <p>取得に失敗しました: {url_mainError.message}</p>}
 
           <form onSubmit={handleSubmit} className="mt-3 flex gap-2">
