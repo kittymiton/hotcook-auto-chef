46d54d9d9b998b034644950e31364854565125b1
kittymiton <kitty.miton@gmail.com>
2026-06-10 20:37:52 +0900

    refactor: Surfaceを共通UI化しレシピ詳細用styleを追加

--- src/app/(authenticated)/talkRoom/_components/talks/TalkRecipe.tsx
+++ src/app/(authenticated)/talkRoom/_components/talks/TalkRecipe.tsx
@@ -1,5 +1,5 @@
 import type { RecipeObj } from '@/lib/schema/recipeBlockSchema';
-import { TalkSurface } from '@authenticated/talkRoom/components/talks/TalkSurface';
+import { Surface } from '../../../../../components/ui/Surface';
 
 type Props = {
   recipe: RecipeObj;
@@ -8,7 +8,7 @@ type Props = {
 // TODO: RecipeDetail実装時に、材料・作り方などの内部表示を共通パーツとして抽出する。
 export const TalkRecipe = ({ recipe }: Props) => {
   return (
-    <TalkSurface type="recipe">
+    <Surface type="recipe">
       <h2 className="mb-1 text-[18px] font-[600]">
         {recipe['レシピタイトル']}
       </h2>
@@ -52,6 +52,6 @@ export const TalkRecipe = ({ recipe }: Props) => {
           ))}
         </ol>
       </div>
-    </TalkSurface>
+    </Surface>
   );
 };
