36f873baf29aeccee9f7e2d087347c4f60be48b5
kittymiton <kitty.miton@gmail.com>
2026-07-28 12:43:43 +0900

    refactor: RecipeListのJSX構造を整理

--- src/app/(authenticated)/recipes/_components/RecipeList.tsx
+++ src/app/(authenticated)/recipes/_components/RecipeList.tsx
@@ -11,33 +11,31 @@ type Props = {
 
 export const RecipeList = ({ recipes }: Props) => {
   return (
-    <>
-      <ul className="space-y-4">
-        {recipes.map((recipe) => (
-          <li key={recipe.id}>
-            <Link href={`/recipes/${recipe.id}`}>
-              <Surface variant="recipe-list-item">
-                <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between">
-                  <section>
-                    <RecipeTitle title={recipe.title} variant="compact" />
-                    <div className="flex items-start gap-1">
-                      {recipe.point && `💡`}
-                      <RecipePoint point={recipe.point} />
-                    </div>
-                    <ul className="mb-1 flex flex-wrap gap-2">
-                      {recipe.tags.map((tag: string) => (
-                        <li key={tag}>
-                          <RecipeTag keyword={tag} />
-                        </li>
-                      ))}
-                    </ul>
-                  </section>
+    <ul className="space-y-4">
+      {recipes.map((recipe) => (
+        <li key={recipe.id}>
+          <Link href={`/recipes/${recipe.id}`}>
+            <Surface variant="recipe-list-item">
+              <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between">
+                <div>
+                  <RecipeTitle title={recipe.title} variant="compact" />
+                  <div className="flex items-start gap-1">
+                    {recipe.point && `💡`}
+                    <RecipePoint point={recipe.point} />
+                  </div>
+                  <ul className="mb-1 flex flex-wrap gap-2">
+                    {recipe.tags.map((tag: string) => (
+                      <li key={tag}>
+                        <RecipeTag keyword={tag} />
+                      </li>
+                    ))}
+                  </ul>
                 </div>
-              </Surface>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </>
+              </div>
+            </Surface>
+          </Link>
+        </li>
+      ))}
+    </ul>
   );
 };
